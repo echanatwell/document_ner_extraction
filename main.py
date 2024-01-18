ee8f74c57b76ab4056b3713860c90fd811259e01
mkoltunov <mkoltunov@ubuntu-gui.myguest.virtualbox.org>
2024-01-18 17:59:26 +0500

    fix requirements.txt and main.py

--- main.py
+++ main.py
@@ -160,4 +160,4 @@ def extract_context(data: Dict[str, str]):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', port=8000, log_level='info', reload=False)
+    uvicorn.run('main:app', port=8000, host='0.0.0.0', log_level='info', reload=False)
